7b058d8264998d9352a027ac2c99b49e5598f14b
BeccaL <103158276+PixelBayBuilds@users.noreply.github.com>
2023-11-29 22:45:06 -0500

    git commit-m "created reusable components"

---
+++ becca-new-app/src/InputWithLabel.js
@@ -0,0 +1,27 @@
+import React, { useRef, useEffect } from "react";
+
+const InputWithLabel = ({ id, children, value, onChange, onSubmit }) => {
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
+
+	return (
+		<>
+			<form onSubmit={onSubmit}>
+				<label htmlFor={id}>{children}</label>
+				<input
+					type="text"
+					name="title"
+					id={id}
+					value={value}
+					onChange={onChange}
+					ref={inputRef}
+				></input>
+				<button type="submit">Add</button>
+			</form>
+		</>
+	);
+};
+export default InputWithLabel;
