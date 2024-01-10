45a6a672211ff91dd474b696dbecff5b6faaafbb
BeccaL <103158276+PixelBayBuilds@users.noreply.github.com>
2024-01-09 23:06:07 -0500

    git commit -m "connected API"

--- becca-new-app/src/App.js
+++ becca-new-app/src/App.js
@@ -2,26 +2,48 @@ import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
+async function fetchData(setTodoList, setIsLoading) {
+	const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`;
+
+	const options = {
+		method: "GET",
+		headers: {
+			Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+		},
+	};
+
+	try {
+		const response = await fetch(url, options);
+
+		if (!response.ok) {
+			throw new Error(`Error: ${response.status}`);
+		}
+
+		const result = await response.json();
+		const fetchedTodoList = result.records || [];
+
+		const todos = fetchedTodoList.map((todo) => ({
+			title: todo.fields.title,
+			id: todo.id,
+		}));
+
+		setTodoList(todos); // Set application's todoList
+
+		setIsLoading(false);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		setIsLoading(false);
+	}
+}
+
 function App() {
 	const [todoList, setTodoList] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				setTimeout(() => {
-					const savedTodoList =
-						JSON.parse(localStorage.getItem("savedTodoList")) || [];
-					setTodoList(savedTodoList);
-					setIsLoading(false);
-				}, 2000);
-			} catch (error) {
-				setIsLoading(false);
-			}
-		};
-
-		fetchData();
+		fetchData(setTodoList, setIsLoading);
 	}, []);
+
 	useEffect(() => {
 		if (!isLoading) {
 			localStorage.setItem("savedTodoList", JSON.stringify(todoList));
